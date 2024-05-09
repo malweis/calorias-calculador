6602253864be7038224acdc1d369c4a42c69d8bd
malweis <58945171+malweis@users.noreply.github.com>
2024-05-09 09:33:21 -0400

    fix: fixes for bun

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
 import { Slot } from 'expo-router';
 import { View, Text, Pressable } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../components/Header';
 
 export default function HomeLayout() {
   return (
+   <SafeAreaProvider>
     <SafeAreaView style={{ flex: 1 }}>
       <Header title='Header' />
       <Slot />
       <Text>testo</Text>
     </SafeAreaView>
+    </SafeAreaProvider> 
   );
 }
